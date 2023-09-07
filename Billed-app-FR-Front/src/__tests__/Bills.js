fe5c47056b81c6242b17080c415a19127f454ec8
Seb59000 <36163352+Seb59000@users.noreply.github.com>
2023-09-07 20:30:24 +0200

    Bug 4 fixed

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -10,6 +10,17 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 
 import router from "../app/Router.js";
 
+// Import the necessary functions and components
+import { render } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import BillsPage from '../BillsPage';
+// Mock your bills data
+// const bills = [
+//   { id: 1, date: '2021-01-01' },
+//   { id: 2, date: '2021-02-01' },
+//   { id: 3, date: '2021-03-01' },
+// ];
+
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
@@ -31,11 +42,29 @@ describe("Given I am connected as an employee", () => {
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      console.log('dates', dates)
+      // console.log('dates', dates)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
-      console.log('datesSorted', datesSorted)
+      // console.log('datesSorted', datesSorted)
       // expect(dates).toEqual(datesSorted)
     })
   })
 })
+describe('BillsPage', () => {
+  it('should display bills ordered from earliest to latest', () => {
+    // Render the BillsPage component
+    render(<BillsPage bills={bills} userRole="employee" />);
+
+    // Get all the bill elements on the page
+    const billElements = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+
+    // Iterate over the bill elements to check the order
+    for (let i = 0; i < billElements.length - 1; i++) {
+      const currentBillDate = billElements[i].getAttribute('data-date');
+      const nextBillDate = billElements[i + 1].getAttribute('data-date');
+
+      // Ensure that the current bill date is earlier or equal to the next bill date
+      expect(new Date(currentBillDate)).toBeLessThanOrEqual(new Date(nextBillDate));
+    }
+  });
+});
--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -86,10 +86,7 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
-    console.log('1 EditTicket', this.counter)
-
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    console.log('2 EditTicket', this.counter)
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
       bills.forEach(b => {
@@ -99,7 +96,6 @@ export default class {
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
       this.counter++
-      console.log('3 EditTicket', this.counter)
     } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
@@ -108,7 +104,6 @@ export default class {
       `)
       $('.vertical-navbar').css({ height: '120vh' })
       this.counter++
-      console.log('3.2 EditTicket', this.counter)
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
@@ -136,33 +131,25 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    console.log('index', index)
-    console.log('this.index', this.index)
-    // console.log('1 ShowTickets', this.counter)
-    // if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.counter === undefined || this.index !== index) this.counter = 0
-    // console.log('2 ShowTickets', this.counter)
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' })
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
       this.counter++
-      // console.log('3 ShowTickets', this.counter)
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' })
       $(`#status-bills-container${this.index}`)
         .html("")
       this.counter++
-      // console.log('3.2 ShowTickets', this.counter)
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).off().click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
-
   }
 
   getBillsAllUsers = () => {
